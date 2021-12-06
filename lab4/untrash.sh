f0cda023b660e345aac67d40a5099d5436fef54b
KhetagAb <31622023+KhetagAb@users.noreply.github.com>
2021-12-07 01:34:30 +0300

    Update untrash.sh

--- lab4/untrash.sh
+++ lab4/untrash.sh
@@ -11,34 +11,34 @@ fi
 
 for i in $(grep "|"$file"|" $log); do
     path=$(echo $i | awk -F '|' '{print $1}')
-    trashId=$(echo $i | awk -F '|' '{print $3}')
+    trash_id=$(echo $i | awk -F '|' '{print $3}')
 
-    if [ -e $trash/$trashId ]; then
+    if [ -e $trash/$trash_id ]; then
         echo "Do you want to recovery $path/$file? (y/n)"
         read answer
 
         if [ $answer == "y" ]; then
-            targetPath=$HOME
+            target_path=$HOME
             if [ -d $path ]; then
-                targetPath=$path
+                target_path=$path
             fi
 
-            fileName=$file
+            file_name=$file
 
-            while [ -e $targetPath/$fileName ]; do
-                echo "File $targetPath/$fileName already exists. Enter new name:"
-                read fileName
+            while [ -e $target_path/$file_name ]; do
+                echo "File $target_path/$file_name already exists. Enter new name:"
+                read file_name
             done
 
-            ln $trash/$trashId $targetPath/$fileName
+            ln $trash/$trash_id $target_path/$file_name
 
-            echo "Saved file $path/$file was moved from trash as $targetPath/$fileName"
+            echo "Saved file $path/$file was moved from trash as $target_path/$file_name"
 
         else
             echo "Saved file $path/$file annihilated from trash"
         fi
 
-        rm $trash/$trashId
+        rm $trash/$trash_id
     else
         echo "Warning: Saved file $path/$file was lost from trash"
     fi
